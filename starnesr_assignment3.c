d69c45d15963785afd0e44e329332baa9d0a0cc9
Reef Starnes <starnesr@oregonstate.edu>
2025-08-03 01:26:28 -0700

    other commands through fork(), exec() & waitpid()

--- starnesr_assignment3.c
+++ starnesr_assignment3.c
@@ -92,9 +92,70 @@ void change_directory(struct command_line *command, const char *home){
         //there has been error
         printf("cannot change directory to %s\n", directory);
     }
+    
+
+
 
 }
 
+int other_commands(struct command_line *command, int *lastStatus){
+    pid_t spawnpid = -5;
+    int childStatus;
+    // int child;
+
+    spawnpid = fork();
+
+    //check if fork failed
+    if (spawnpid == -1)
+    {
+        perror("fork() failed!");
+        //return error code
+        return 1;
+    }
+
+    /*Recall: fork returns different things to the fork and the child.
+    To the child it return returns 0, and to the parent it returns the process ID of the child*/ 
+
+
+    //check if child process is running
+    if (spawnpid == 0)
+    {
+        execvp(command->argv[0], command->argv);
+        //if the child process is active here, execvp() must of failed
+        perror("execvp() failed");
+        free_command(command); 
+        exit(1);
+    }
+    
+    /*
+    personal notes about execvp():
+    we fork to run execvp() because execvp() replaces the current process by running a compiled program based on its arguments.
+    By calling execvp in the child, the main process "the parent", stays alive. 
+
+    personal notes about waitpid():
+    waits for a specific child process (identified by its Process ID) and returns a stat value analyzed with "Analysis Macros"
+
+    */
+    
+    //wait for child process to finish
+    waitpid(spawnpid, &childStatus, 0);
+
+    if (WIFEXITED(childStatus)){
+        *lastStatus = WEXITSTATUS(childStatus);
+    } else if (WIFSIGNALED(childStatus)){
+        *lastStatus = WTERMSIG(childStatus);
+    }
+
+    return 0;
+    
+
+
+
+}
+
+void status(){
+
+}
 
 
 int main(){
@@ -135,15 +196,13 @@ int main(){
 
         else if (strcmp(currCommand->argv[0], "status") == 0) 
         {
-            //status(lastStatus);
+            status(lastStatus);
             free_command(currCommand);
             continue;
         }
 
         else{
-            
-
-
+            other_commands(currCommand, &lastStatus);
 
         }
 
@@ -166,6 +225,8 @@ sources:
 https://www.geeksforgeeks.org/c/fork-system-call/ //for forking
 
 Canvas "Exploration: Process API - Monitoring Child Processes" //for forking
+
+https://www.ibm.com/docs/en/zvm/7.4.0?topic=descriptions-waitpid-wait-specific-child-process-end //for waitpid()
 
 //https://www.geeksforgeeks.org/cpp/cpp-getenv-function/ //for acquiring home directory
 
